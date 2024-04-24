8d9464e6dc92da53c5661bd758218750d67e2554
Giulio Tonielli <70532912+giuliotonielli@users.noreply.github.com>
2024-04-24 13:40:19 +0200

    Update helpers2.py

--- helpers2.py
+++ helpers2.py
@@ -98,6 +98,90 @@ def get_ter(isin):
 
     return float(ter)
 
+def createURL(url, name):
+    '''
+    Given a url and name of an index it creates the correspondant url
+    Parameters: url, name (Strings)
+    Returns: url (String)
+    '''
+
+    for word in name.split():
+        if '®' in word:
+            word=word[0:-1]
+
+        # & letter cannot be part of a link, %26 to substitute
+        if word == "S&P":
+            word = "S%26P"
+
+        url += word + "%20"
+    return url[:-3] + ".csv"
+
+def get_index_data(name):
+        try:                
+            url = createURL("https://raw.githubusercontent.com/NandayDev/MSCI-Historical-Data/main/countries/", name)
+            data = urlopen(url)
+        except:
+            try:
+                url = createURL("https://raw.githubusercontent.com/NandayDev/MSCI-Historical-Data/main/curvo/", name)
+                data = urlopen(url)
+            except:
+                try:
+                    url = createURL("https://raw.githubusercontent.com/NandayDev/MSCI-Historical-Data/main/countries_small_cap/", name)
+                    data = urlopen(url)
+                except:
+                    try:
+                        url = createURL("https://raw.githubusercontent.com/NandayDev/MSCI-Historical-Data/main/indexes_gross/", name)
+                        data = urlopen(url)
+                    except:             
+                        url = createURL("https://raw.githubusercontent.com/NandayDev/MSCI-Historical-Data/main/regions_small_cap/", name)
+                        data = urlopen(url)
+        return data
+
+def get_index_and_etf_data(portfolio_tickers, index_names):
+        '''
+        Given the portfolio_tickers and index_names lists the function gets the correspondant index name of each ETF.
+        Then, it joins the older index data to the newer ETF data month by month, so that we have more historical data
+        for ETFs. It returns the portfolio_prices dataframe with the older index data added to it.
+
+        Parameters:
+        - portfolio_tickers [List of Strings]
+        - index_names [List of Strings]
+
+        Returns:
+        - portfolio_prices [Dataframe]
+        '''
+        portfolio_tickers.append("IBM")
+        index_names.append("")
+        all_stocks = yf.download(portfolio_tickers, interval='1mo')['Open']
+        portfolio_prices = pd.DataFrame()
+        for ticker in portfolio_tickers:
+            price = all_stocks[ticker]
+            portfolio_prices[ticker] = price
+
+
+        for i in range(0,len(portfolio_tickers)):
+            name = index_names[i]
+            if name!="":
+                data = get_index_data(name)
+
+                ticker = portfolio_tickers[i]
+                return_data = pd.read_csv(data, sep=",", names=["Date", ticker], skiprows=1)
+                return_data["Date"] += "-01"
+
+                for i in range(0,len(return_data)):
+                        return_data.loc[i,"Date"] = datetime.strptime(return_data.loc[i,"Date"], '%Y-%m-%d')
+
+                return_data.set_index("Date", inplace = True)
+                portfolio_prices["^"+ticker] = return_data[ticker]
+                first_valid_index = portfolio_prices[ticker].index.get_loc(portfolio_prices[ticker].first_valid_index())
+                portfolio_prices[ticker].fillna(return_data[ticker]*portfolio_prices[ticker][first_valid_index]/portfolio_prices["^"+ticker][first_valid_index], inplace = True)
+                portfolio_prices.drop("^"+ticker, axis=1, inplace = True)
+        
+        portfolio_prices.drop("IBM", axis=1, inplace = True)
+        portfolio_prices.dropna(axis = 0, how = 'all', inplace = True)
+
+        return portfolio_prices
+    
 
 def annual_portfolio_return(portfolio_tickers, portfolio_weight):
     '''
