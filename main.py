260cccb4adf1f2555a599ba8293c75655d85bb4b
Giulio Tonielli <70532912+giuliotonielli@users.noreply.github.com>
2024-05-12 16:04:18 +0200

    updated main

--- main.py
+++ main.py
@@ -3,12 +3,11 @@
 from helpermodules.portfolio_helpers import *
 from helpermodules.asset_helpers import *
 
-asset1 = Asset("ETF", "VUAA", "Vanguard S&P 500 UCITS ETF (USD) Accumulating")
+asset1 = Asset("ETF", "VUAA.MI", "Vanguard S&P 500 UCITS ETF (USD) Accumulating")
 asset1.load()
 
-asset2 = Asset("ETF", "EIMI.MI", "iShares Core MSCI Emerging Markets IMI UCITS ETF (Acc)")
+asset2 = Asset("ETF", "EIMI.MI", "MSCI Emerging Markets IMI")
 asset2.load()
-asset2.info()
 
 assets = [asset1, asset2]
 portfolio = Portfolio(assets, [0.3, 0.7])
@@ -16,3 +15,4 @@ portfolio = Portfolio(assets, [0.3, 0.7])
 print(portfolio.df)
 print(portfolio.portfolio_return_pac(1000, 100, 0.1, True,startdate="2009-10-01", enddate="2018-09-09" ))
 print(portfolio.monthly_portfolio_return().to_string())
+print(portfolio.graph_returns_frequency())
