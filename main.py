b82a4b9ee9c23ca355c172230dcf68b89af29d35
Giulio Tonielli <70532912+giuliotonielli@users.noreply.github.com>
2024-05-23 12:53:18 +0200

    Added roadmap, modified helper mods

--- main.py
+++ main.py
@@ -17,14 +17,14 @@ asset2.load_df()
 
 
 assets = [asset1, asset2]
-weights = [1.0, 0.0]
+weights = [0.5, 0.5]
 portfolio = Portfolio(assets, weights)
 
 
-#print(portfolio.df)
-#print(portfolio.portfolio_return_pac(1000, 0, 0, True, startdate="2020-01-01", enddate="2022-01-01"))
-#print(portfolio.monthly_portfolio_return().to_string())
-#print(portfolio.graph_returns_frequency())
+print(portfolio.df.to_string())
+print(portfolio.portfolio_return_pac(1000, 0, 0, True, startdate="2020-01-01", enddate="2022-01-01"))
+print(portfolio.monthly_portfolio_return().to_string())
+print(portfolio.graph_returns_frequency())
 
 
 
