62bb2fa5a92794584af31ce0e96f9f7111a67127
Alberto Toia <albetoiaa@gmail.com>
2024-05-24 00:04:38 +0200

    first frontend prototype

--- search.js
+++ search.js
@@ -1,25 +1,29 @@
+// Variabile per salvare l'ISIN selezionato
+let selectedISIN = '';
+ 
 // Funzione per eseguire la ricerca fuzzy e visualizzare i risultati
 function performSearch(query, data) {
     const fuse = new Fuse(data, {
-        keys : ['ISIN', 'FUNDNAME', 'TICKER'],
+        keys: ['ISIN', 'FUNDNAME', 'TICKER'],
         includeScore: true,
         threshold: 0.3
     });
-
-    const result = fuse.search(query);
-
-    console.log(result)
-
+ 
+    const result = fuse.search(query, {limit:4});
+ 
     const searchResults = document.getElementById('search-results');
     searchResults.innerHTML = '';
-
+ 
     result.forEach(item => {
-        const div = document.createElement('div');
-        div.textContent = `ISIN: ${item.item.ISIN}, FUNDNAME: ${item.item.FUNDNAME},  TICKER: ${item.item.TICKER}`;
-        searchResults.appendChild(div);
+        const option = document.createElement('option');
+        option.value = item.item.ISIN;
+        option.textContent = `ISIN: ${item.item.ISIN}, FUNDNAME: ${item.item.FUNDNAME}, TICKER: ${item.item.TICKER}`;
+        searchResults.appendChild(option);
     });
+ 
+    searchResults.style.display = result.length > 0 ? 'block' : 'none';
 }
-
+ 
 // Funzione per gestire l'evento di input sulla barra di ricerca
 function handleSearchInput() {
     const query = document.getElementById('search-input').value;
@@ -28,6 +32,22 @@ function handleSearchInput() {
         .then(data => performSearch(query, data))
         .catch(error => console.error('Errore durante il recupero dei dati:', error));
 }
-
+ 
 // Aggiungi un listener per l'evento di input sulla barra di ricerca
 document.getElementById('search-input').addEventListener('input', handleSearchInput);
+ 
+// Aggiungi un listener per l'evento di cambio sul select
+document.getElementById('search-results').addEventListener('change', function() {
+    const selectedOption = this.options[this.selectedIndex];
+    selectedISIN = selectedOption.value; // Salva l'ISIN selezionato
+    document.getElementById('selected-result').textContent = `You selected: ${selectedOption.textContent}`;
+});
+ 
+// Aggiungi un listener per l'evento di change sul select
+document.getElementById('search-results').addEventListener('change', function() {
+    const selectedOption = this.options[this.selectedIndex];
+    selectedISIN = selectedOption.value; // Salva l'ISIN selezionato
+    document.getElementById('selected-result').textContent = `You selected: ${selectedOption.textContent}`;
+    console.log(selectedISIN); // Stampa l'ISIN selezionato
+    document.getElementById('ISIN').value = selectedISIN
+});
