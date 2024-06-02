6a8ef43de71a560818f24127fff889e02d0aa12e
Giulio Tonielli <70532912+giuliotonielli@users.noreply.github.com>
2024-06-02 12:37:37 +0200

    fixed MDD

--- main.py
+++ main.py
@@ -21,10 +21,11 @@ weights = [0.5, 0.5]
 portfolio = Portfolio(assets, weights)
 
 
-#print(portfolio.df.to_string())
-#print(portfolio.portfolio_return_pac(1000, 0, 0, True, startdate="2020-01-01", enddate="2022-01-01"))
-#print(portfolio.monthly_portfolio_return().to_string())
-#print(portfolio.graph_returns_frequency())
+print(portfolio.df)
+print('Maximum drawdown: ', portfolio.MDD('1990-01-01', '2023-01-01'))
+print(portfolio.portfolio_return_pac(1000, 0, 0, True, startdate="2020-01-01", enddate="2022-01-01"))
+print(portfolio.monthly_portfolio_return().to_string())
+print(portfolio.graph_returns_frequency())
 
 
 
